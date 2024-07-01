a7cdb93ab217492826b9b6d73185ff24994a558b
GatsinziMeek <hackergastinzi@gmail.com>
2024-07-01 02:39:03 +0200

    new feature added

---
+++ app/hooks/useOtherUser.ts
@@ -0,0 +1,19 @@
+import { User } from "@prisma/client"
+import { FullConversationType } from "../types"
+import { useSession } from "next-auth/react"
+import { useMemo } from "react";
+
+const useOtherUser = (Conversation: FullConversationType | {users: User[]}) => {
+    const session = useSession();
+
+    const otherUser = useMemo(() => {
+        const currentUserEmail = session?.data?.user?.email;
+
+        const otherUser = Conversation.users.filter(user => user.email === currentUserEmail);
+
+        return otherUser
+    },[ session?.data?.user?.email, Conversation.users ])
+
+    return otherUser[0];
+}
+export default useOtherUser
--- app/types/index.ts
+++ app/types/index.ts
@@ -7,5 +7,5 @@ export type FullMessageType = Message & {
 
 export type FullConversationType = Conversation & {
     users: User[],
-    message: FullConversationType[],
+    message: FullMessageType[],
 };
